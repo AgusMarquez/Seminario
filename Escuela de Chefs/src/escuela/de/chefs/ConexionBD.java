c31b502caab8507a0dfca2214ca196c95dd05028
Agustín Exequiel Márquez Moyano <144951075+AgusMarquez@users.noreply.github.com>
2024-06-30 21:09:41 -0300

    Update ConexionBD.java
    ConexionBD actualizada para conectar a la base de datos escuela_chefs

--- Escuela de Chefs/src/escuela/de/chefs/ConexionBD.java
+++ Escuela de Chefs/src/escuela/de/chefs/ConexionBD.java
@@ -5,11 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/?user=root";
-    private static final String USER = "root"; 
-    private static final String PASSWORD = "0301"; 
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/escuela_chefs?user=root&password=0301";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL);
     }
 }
